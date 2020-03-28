11ea88aad524f21d5c41dd0e1fbf5d15b8c70fca
Paul <trestian.pau07@gmail.com>
2020-03-28 17:23:09 +0200

    added encrypted password in test data

--- api/src/main/resources/db/data/afterMigrate.sql
+++ api/src/main/resources/db/data/afterMigrate.sql
@@ -1,3 +1,3 @@
 USE nextdoor;
 -- USER
-INSERT IGNORE INTO user VALUES ('3b13da2d-0ff8-4388-88fb-5182b4e4ec42','2019-12-18 13:17:17','test@test.test','password'); -- Bcrypt encrypted: $2y$12$UrKngInmATHE5X4PPL/oEuqzWi.nmVsQLhd94lUoBGDTKaqzwJv8W
+INSERT IGNORE INTO user VALUES ('3b13da2d-0ff8-4388-88fb-5182b4e4ec42','2019-12-18 13:17:17','test@test.test','$2y$12$UrKngInmATHE5X4PPL/oEuqzWi.nmVsQLhd94lUoBGDTKaqzwJv8W'); --Decrypted password : 'password'
