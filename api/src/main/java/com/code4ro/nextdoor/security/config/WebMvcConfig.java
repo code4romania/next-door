ec591606b0d625fa6bb4573b3f9843378df82b53
George Bejan <George.Bejan@cloudflight.io>
2020-03-29 20:45:54 +0300

    Fix CORS

---
+++ api/src/main/java/com/code4ro/nextdoor/security/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.code4ro.nextdoor.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private static final long MAX_AGE_SECS = 3600;
+
+    /**
+     * allow cross origin requests from the client
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+                .maxAge(MAX_AGE_SECS);
+    }
+}
